32b4acdc1e7b14161ef6af3c6bcbf44b6ad86080
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-02-12 15:50:52 +0900

    Add inital file

---
+++ nrf24l01+.c
@@ -0,0 +1,27 @@
+/**
+ * File:  nrf24l01+.c
+ * Author: ymiyamoto
+ *
+ * Created on Mon Feb 12 13:47:32 2018
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+
+#define DRIVER_NAME "nrf24l01"
+
+static int nrf24l01_init(void)
+{
+  pr_info("%s initalize", DRIVER_NAME);
+  return 0;
+}
+
+static void nrf24l01_exit(void)
+{
+  pr_info("%s exit", DRIVER_NAME);
+}
+
+module_init(nrf24l01_init);
+module_exit(nrf24l01_exit);
+
+MODULE_AUTHOR("MIYAMOTO Yohei");
+MODULE_LICENSE("GPL v2");
